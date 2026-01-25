a93f44d3966fc2f033ef71d3cbd8068cddff8aeb
Júlia Garcia <julia.garciasilva@hotmail.com>
2026-01-25 18:32:04 -0300

    feat: passando props para os cards de receitas

--- src/app/components/RecipeCard/index.tsx
+++ src/app/components/RecipeCard/index.tsx
@@ -1,22 +1,23 @@
+import { Recipe } from "@/app/lib/data";
 import Image from "next/image";
 import Link from "next/link";
 
 
-export default function RecipeCard() {
+export default function RecipeCard({ recipe }: RecipeCardProps) {
   return (
-    <Link href="">
+    <Link href={`/receitas/${recipe.id}`}>
       <div className="border border-slate-200 rounded-lg overflow-hidden shadow-sm hover:shadow-md transition-shadow">
         <div className="relative h-48 w-full">
           <Image 
-          src="/receitas/salada-caprese.png" 
+          src={recipe.image} 
           fill 
-          alt="Título da receita"
+          alt={recipe.title}
           />
         </div>
 
         <div className="flex flex-col p-4 gap-2">
-            <h3 className="text-lg font-bold">Título da receita</h3>
-            <p>Descrição da receita</p>
+            <h3 className="text-lg font-bold">{recipe.title}</h3>
+            <p>{recipe.description}</p>
         </div>
       </div>
     </Link>
