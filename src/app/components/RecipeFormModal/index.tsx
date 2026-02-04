9656f234a45c9edd61a4461095fd13b82a15e4ef
Júlia Garcia <julia.garciasilva@hotmail.com>
2026-02-03 21:24:51 -0300

    feat: setup do RHF e yup

--- src/app/components/RecipeFormModal/index.tsx
+++ src/app/components/RecipeFormModal/index.tsx
@@ -1,4 +1,10 @@
+import {
+  RecipeFormData,
+  recipeSchema,
+} from "@/app/lib/formValidationSchemas/recipeShema";
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "../ui/dialog";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 interface RecipeFormModalProps {
   isOpen: boolean;
@@ -9,6 +15,22 @@ export default function RecipeFormModal({
   isOpen,
   onClose,
 }: RecipeFormModalProps) {
+  const {
+    register,
+    reset,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<RecipeFormData>({
+    resolver: yupResolver(recipeSchema),
+    mode: "onSubmit",
+  });
+
+  const onSubmit = (data: RecipeFormData) => {
+    console.log(data);
+    reset();
+    onClose
+  };
+
   const inputStyle = "p-2 border border-zinc-200 rounded-md";
 
   return (
@@ -18,57 +40,144 @@ export default function RecipeFormModal({
           <DialogTitle>Nova Receita</DialogTitle>
         </DialogHeader>
 
-        <form className="flex flex-col gap-4 w-full">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col gap-4 w-full"
+        >
           <div className="grid grid-cols-2 gap-2">
             {/* Título */}
             <div className="flex flex-col gap-1">
               <label htmlFor="title">Título</label>
-              <input className={inputStyle} type="text" id="title" />
+              <input
+                className={inputStyle}
+                type="text"
+                id="title"
+                {...register("title")}
+              />
+              {errors.title && (
+                <span className="text-sm text-red-500">
+                  {errors.title.message}
+                </span>
+              )}
             </div>
 
             {/* Categoria */}
             <div className="flex flex-col gap-1">
               <label htmlFor="category">Categoria</label>
-              <input className={inputStyle} type="text" id="category" />
+              <input
+                className={inputStyle}
+                type="text"
+                id="category"
+                {...register("category")}
+              />
+              {errors.category && (
+                <span className="text-sm text-red-500">
+                  {errors.category.message}
+                </span>
+              )}
             </div>
           </div>
 
           {/* Descrição */}
           <div className="flex flex-col gap-1">
             <label htmlFor="description">Descrição</label>
-            <textarea className={inputStyle} id="description" />
+            <textarea
+              className={inputStyle}
+              id="description"
+              {...register("description")}
+            />
+            {errors.description && (
+              <span className="text-sm text-red-500">
+                {errors.description.message}
+              </span>
+            )}
           </div>
 
-            {/* URL imagem */}
+          {/* URL imagem */}
           <div className="flex flex-col gap-1">
             <label htmlFor="imageUrl">URL da Imagem</label>
-            <input type="text" className={inputStyle} id="imageUrl" placeholder="/placeholder.svg" />
+            <input
+              type="text"
+              className={inputStyle}
+              id="imageUrl"
+              placeholder="/placeholder.svg"
+              {...register("imageURL")}
+            />
+            {errors.imageURL && (
+              <span className="text-sm text-red-500">
+                {errors.imageURL.message}
+              </span>
+            )}
           </div>
 
           <div className="grid grid-cols-3 gap-2">
             {/* Tempo de preparo*/}
             <div className="flex flex-col gap-1">
               <label htmlFor="prepTime">Tempo de Preparo</label>
-              <input className={inputStyle} type="text" id="prepTime" placeholder="30 minutos"/>
+              <input
+                className={inputStyle}
+                type="text"
+                id="prepTime"
+                placeholder="30 minutos"
+                {...register("prepTime")}
+              />
+              {errors.prepTime && (
+                <span className="text-sm text-red-500">
+                  {errors.prepTime.message}
+                </span>
+              )}
             </div>
 
             {/* Tempo de cozimento */}
             <div className="flex flex-col gap-1">
               <label htmlFor="cookTime">Tempo de Cozimento</label>
-              <input className={inputStyle} type="text" id="cookTime" placeholder="15 minutos"/>
+              <input
+                className={inputStyle}
+                type="text"
+                id="cookTime"
+                placeholder="15 minutos"
+                {...register("cookTime")}
+              />
+              {errors.cookTime && (
+                <span className="text-sm text-red-500">
+                  {errors.cookTime.message}
+                </span>
+              )}
             </div>
 
             {/* Porções */}
             <div className="flex flex-col gap-1">
               <label htmlFor="servings">Porções</label>
-              <input className={inputStyle} type="number" id="servings" defaultValue={1}/>
+              <input
+                className={inputStyle}
+                type="number"
+                id="servings"
+                defaultValue={1}
+                {...register("servings")}
+              />
+              {errors.servings && (
+                <span className="text-sm text-red-500">
+                  {errors.servings.message}
+                </span>
+              )}
             </div>
           </div>
 
-            <div className="flex gap-2 self-end">
-                <button type="button" onClick={onClose} className="bg-white border border-zinc-300 rounded-md hover:bg-gray-100 transition-colors px-4 py-2 font-medium">Cancelar</button>
-                <button type="submit" className="bg-black text-white rounded-md hover:bg-gray-800 transition-colors px-4 py-2 font-medium">Criar receita</button>
-            </div>
+          <div className="flex gap-2 self-end">
+            <button
+              type="button"
+              onClick={onClose}
+              className="bg-white border border-zinc-300 rounded-md hover:bg-gray-100 transition-colors px-4 py-2 font-medium"
+            >
+              Cancelar
+            </button>
+            <button
+              type="submit"
+              className="bg-black text-white rounded-md hover:bg-gray-800 transition-colors px-4 py-2 font-medium"
+            >
+              Criar receita
+            </button>
+          </div>
         </form>
       </DialogContent>
     </Dialog>
