4303b0e4716f3f523e69fd4dbe4f52a207a4a8bf
Júlia Garcia <julia.garciasilva@hotmail.com>
2026-02-05 22:15:15 -0300

    fix: exibicao dos cards de receita na tela inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,30 @@
+"use client"
+
 import { ChevronRight } from "lucide-react";
 import Link from "next/link";
-import{ recipes } from "@/app/lib/data";
 import RecipeCard from "@/app/components/RecipeCard";
-
+import { useEffect, useState } from "react";
+import { Recipe } from "@/app/lib/data";
+import api from "@/app/lib/api";
+import { toast } from "sonner";
 
 export default function Home() {
-const featuredRecipes = recipes.slice(0, 3);
+  const [recipes, setRecipes] = useState<Recipe[]>([]);
+
+  useEffect(() => {
+    const fetchRecipes = async () => {
+      try {
+        const response = await api.get("/recipes");
+
+        setRecipes(response.data.slice(0, 3));
+      } catch (error) {
+        console.error("Erro ao requisitar as receitas", error);
+        toast.error("Erro ao requisitar as receitas, tente novamente mais tarde")
+      }
+    };
+
+    fetchRecipes();
+  }, []);
 
   return (
     <main className="flex-grow">
@@ -13,15 +32,10 @@ const featuredRecipes = recipes.slice(0, 3);
       {/* seção Hero */}
       <section className="bg-orange-50 py-12">
         <div className="flex flex-col gap-6 items-center container mx-auto">
-          <h1 className="text-5xl font-bold">Receitas Deliciosas</h1>
-          <p className="text-xl">
-            Descubra receitas simples e saborosas para todas as ocasiões
-          </p>
+          <h1 className="text-5xl font-bold">Receitas deliciosas</h1>
+          <p className="text-xl">Descubra receitas simples e saborosas para todas as ocasiões</p>
 
-          <Link
-            className="bg-orange-500 hover:bg-orange-700 transition-colors text-white font-bold rounded-lg px-3 py-2"
-            href="/receitas"
-          >
+          <Link className="bg-orange-500 hover:bg-orange-700 transition-colors text-white font-bold rounded-lg px-3 py-2" href="/receitas">
             Ver todas as receitas
           </Link>
         </div>
@@ -32,9 +46,9 @@ const featuredRecipes = recipes.slice(0, 3);
         <div className="flex flex-col items-center container mx-auto gap-8">
           <h2 className="text-lg font-bold">Receitas em destaque</h2>
 
-          <div className="flex w-full gap-8">
-            {featuredRecipes.map((recipe) => (
-              <RecipeCard key={recipe.id} recipe={recipe} />
+          <div className="grid grid-cols-3 w-full gap-8">
+            {recipes.map((recipe) => (
+              <RecipeCard key={recipe.id} recipe={recipe}/>
             ))}
           </div>
 
